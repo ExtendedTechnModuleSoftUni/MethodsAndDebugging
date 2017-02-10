67c9ab21cc2b69fc08338270af48bff3151d18ab
VenciSarov <venci.sarov@gmail.com>
2017-02-10 07:52:35 +0200

    Last task and solution from Methods was added. Also there is attached file with problem in task conditions!

--- MethodsAllTasks/12E. String Encryption/StringEncryption.cs
+++ MethodsAllTasks/12E. String Encryption/StringEncryption.cs
@@ -6,7 +6,40 @@
     {
         static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
 
+            Console.WriteLine(string.Join("", Encrypt(n)));
+        }
+
+        static string[] Encrypt(int n)
+        {
+            string[] result = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+
+                byte charToByte = (byte)(symbol);
+                byte firstDigit = 0;
+                byte lastDigit = 0;
+
+                if (charToByte < 100)
+                {
+                    firstDigit = (byte)(charToByte / 10);
+                    lastDigit = (byte)(charToByte % 10);
+                }
+                else
+                {
+                    firstDigit = (byte)(charToByte / 100);
+                    lastDigit = (byte)(charToByte % 10);
+                }
+
+                string middlePart = firstDigit.ToString() + lastDigit.ToString();
+
+                result[i] += (char)(charToByte + lastDigit) + middlePart + (char)(charToByte - firstDigit);
+            }
+
+            return result;
         }
     }
 }
